34c6923e6128d551c69cea983cb57eab7ca78ee4
root <root@master-merb.(none)>
2009-11-15 23:43:10 +0000

    first commit, sort of works, need to write tests

---
+++ application.rb
@@ -0,0 +1,79 @@
+require 'lib/priz.rb'
+require '/root/creds.rb'
+require 'right_aws'
+
+class Priz < Merb::Controller
+
+  def _template_location(action, type = nil, controller = controller_name)
+    controller == "layout" ? "layout.#{action}.#{type}" : "#{action}.#{type}"
+  end
+
+  def index
+	@user_name = cookies[:user_name]
+	@index_name = cookies[:index_name]
+    render
+  end
+
+  
+end
+class Estimate  < Merb::Controller
+
+  def _template_location(action, type = nil, controller = controller_name)
+    controller == "layout" ? "layout.#{action}.#{type}" : "#{action}.#{type}"
+  end
+
+  def index
+	@user_name = cookies[:user_name]
+    render :estimate
+  end
+  def create
+	user_name = params['user_name']
+        customer_name = params['customer_name']
+        quote_name =  params['quote_name']
+        index_name = "#{user_name}-#{customer_name}-#{quote_name}"
+	cookies[:user_name] = user_name
+	cookies[:index_name] = index_name
+	key,skey = getCreds()
+	@sdb = RightAws::SdbInterface.new(key,skey)
+	@sdb.put_attributes('dynamic_usage',index_name,"empty")
+    render
+  end
+end
+
+class NewConfig < Merb::Controller
+
+  def _template_location(action, type = nil, controller = controller_name)
+    controller == "layout" ? "layout.#{action}.#{type}" : "#{action}.#{type}"
+  end
+  def show_daily_price
+         @dy = DyModel.new
+	@index_name = cookies[:index_name]
+	 key,skey = getCreds()
+        @sdb = RightAws::SdbInterface.new(key,skey)
+        render
+  end
+  def index
+	 @dy = DyModel.new
+    render :new_config
+  end
+  def add_config
+	@index_name = cookies[:index_name]
+	key,skey = getCreds()
+	@sdb = RightAws::SdbInterface.new(key,skey)
+	name = params['name']
+	@sdb.put_attributes("dynamic_usage_configs","#{@index_name}-#{name}",params)
+	render :index
+  end
+
+end
+class DailyModel < Merb::Controller
+
+  def _template_location(action, type = nil, controller = controller_name)
+    controller == "layout" ? "layout.#{action}.#{type}" : "#{action}.#{type}"
+  end
+  def index
+         @dy = DyModel.new
+	@daily_model = @dy.get_daily_usage
+    render :edit_daily_model
+  end
+end
---
+++ lib/priz.rb
@@ -0,0 +1,168 @@
+
+require 'yaml'
+class DyModel
+#load 'priz.rb';dy = DyModel.new
+attr_accessor :model,:ami_types,:configs
+def initialize
+	@configs = {}
+	@ami_types = get_types
+end
+def get_default_daily_usage
+       model = open('default_daily_usage.yaml') { |f| YAML.load(f) }
+end
+def get_default_monthly_usage
+	model = open('default_monthly_usage.yaml') { |f| YAML.load(f) }
+end
+def get_monthly_usage
+	fname = 'default_monthly_usage.yaml'
+	if (File.exist?(fname))
+                model = open(fname) { |f| YAML.load(f) }
+        else
+                model = get_default_daily_usage
+        end
+end
+def get_daily_usage
+	fname = 'daily_usage.yaml'
+	if (File.exist?(fname))
+		model = open(fname) { |f| YAML.load(f) }	
+	else
+		model = get_default_daily_usage
+	end
+end
+def save_daily_usage
+	open('daily_usage.yaml', 'w') { |f| YAML.dump(model, f) }	
+end
+def get_types
+	@ami_types = open('ami_types.yaml') { |f| YAML.load(f) }
+end
+def new_config(name)
+	@configs[name] = {:type => :m1small, :min_q => 2,:max_q => 50,:metric => 50,:days_of_week => 5,:weekend_usage => 0.1,:peak_metric => 50}	
+end
+def calc_monthly_optimized_cost(config)
+	daily_matrix = calc_daily_matrix(config)
+	total_instance_hours_per_week = calc_total_weekly_hours(daily_matrix,config)
+	optimal_RIs = calc_optimal_ri(daily_matrix,total_instance_hours_per_week)
+	puts "Optimal RIs: #{optimal_RIs} "
+	weekly_optimized_price = calc_ri_optimized_price(optimal_RIs,daily_matrix,config)
+	weekly_unoptimized_price = calc_unoptimized_weekly_cost(daily_matrix,config)
+	savings = weekly_unoptimized_price.to_f - weekly_optimized_price.to_f
+	puts "Optimized Weekly Price: #{weekly_optimized_price} Unoptimized Weekly Price #{weekly_unoptimized_price} Savings: #{savings}"
+end
+def calc_daily_matrix(config)
+	ami_type = config[:type]
+	daily_model = get_daily_usage	
+	daily_matrix = {}
+	# TODO fix below
+	peak_metric = config[:peak_metric]
+	minimum = config[:min_q]
+	metric = config[:metric]
+	for hour in daily_model.keys
+		next if hour == "timezone"
+		usage = daily_model[hour]
+		# TODO fix hardcode
+		metric_calculation = (peak_metric / metric * usage.to_f).to_f.round
+		# set number of instances to the config minimum unless the calulation is smaller then the minimum
+		metric_calculation <= minimum.to_i ? number_of_instances = minimum : number_of_instances = metric_calculation
+		#puts "Instances calculated: #{metric_calculation} required for hour: #{hour} - #{number_of_instances}"
+		ami_type = config[:type]
+		daily_matrix[hour] = {:usage => usage, :number_of_instances => number_of_instances}
+	end
+	return daily_matrix
+end
+def calc_unoptimized_weekly_cost(daily_matrix,config)
+	od_cost = 0.0
+	ami_type = config[:type]
+
+	# normal usage
+	for hour in daily_matrix.keys	
+		number_of_instances = daily_matrix[hour][:number_of_instances].to_i	
+		od_hourly = @ami_types[ami_type][:OD_hourly]
+                hourly_cost = od_hourly * number_of_instances
+                od_cost += hourly_cost.to_f
+	end	
+	days_of_week = config[:days_of_week]
+	od_cost = od_cost * days_of_week
+	# weekend usage
+        weekend_days = 7 - days_of_week
+        puts "#{weekend_days.to_i} * #{config[:min_q]} * 24 * #{@ami_types[config[:type]][:OD_hourly]}"
+        weekend_cost = weekend_days.to_i * config[:min_q] * 24 * @ami_types[config[:type]][:OD_hourly]
+        puts "Weekend Cost: #{weekend_cost}"		
+	od_cost += weekend_cost
+end
+def calc_optimal_ri(daily_matrix,total_instance_hours_per_week)
+	# TODO: fix this hack, average is not the optimal, not accounting for weekends
+	counter = 0
+        daily_instance_hours = 0
+	for hour in daily_matrix.keys
+                daily_instance_hours += daily_matrix[hour][:number_of_instances].to_i
+		counter += 1
+        end
+	puts "daily_instance_hours  = #{daily_instance_hours} / counter: #{counter}"
+	average_normal_instances = daily_instance_hours / counter
+end
+def calc_ri_optimized_price(optimal_RIs,daily_matrix,config)
+	# TODO: Fix hardcode
+	daily_instance_hours = 0
+	cost = 0.0
+
+	#
+	# calculate normal days
+	#
+	ri_optimized_daily_matrix = {}
+	ami_type = @ami_types[config[:type].to_sym]
+        for hour in daily_matrix.keys
+                instances = daily_matrix[hour][:number_of_instances].to_i
+		if (instances > optimal_RIs)
+			od_instances = instances.to_i - optimal_RIs.to_i
+			hour_price = optimal_RIs * ami_type[:RI_hourly].to_f
+			#hour_price += od_instances * @ami_types[config[:type]][:OD_hourly]
+			hour_price += od_instances * ami_type[:OD_hourly].to_f
+			print "-#{optimal_RIs}-#{od_instances} $#{hour_price}- "
+			cost += hour_price
+			ri_optimized_daily_matrix[hour] = hour_price
+		else
+			hour_price = instances *  ami_type[:RI_hourly].to_f
+			print " *#{instances} $#{hour_price}* "
+			cost += hour_price
+			ri_optimized_daily_matrix[hour] = hour_price
+		end
+        end	
+	weekend_usage = config[:weekend_usage]
+	days_of_week = config[:days_of_week].to_i
+	cost = cost * days_of_week
+
+	#
+        # calculate weekend type days, assume all RIs
+        #
+
+	weekend_usage = config[:weekend_usage]
+        puts "Weekly Cost: #{cost}"
+	weekend_days = 7 - days_of_week
+	puts "#{weekend_days.to_i} * 24 * #{config[:min_q]} * #{ami_type[:RI_hourly]}"
+	weekend_cost = weekend_days.to_i * 24 * config[:min_q].to_i * ami_type[:RI_hourly].to_f
+	puts "Weekend Cost: #{weekend_cost}"
+	cost += weekend_cost
+	return cost,ri_optimized_daily_matrix
+end
+def calc_total_weekly_hours(daily_matrix,config)
+	daily_instance_hours = 0
+	puts "daily matrix count: #{daily_matrix.size}"
+	for hour in daily_matrix.keys	
+		daily_instance_hours += daily_matrix[hour][:number_of_instances].to_i
+	end
+	puts "daily matrix count: #{daily_matrix.size} daily hours: #{daily_instance_hours}"
+	# TODO: fix hardcode
+	days_of_week = config[:days_of_week].to_i
+	weekend_usage = config[:weekend_usage].to_f
+	weekend_days = 7 - days_of_week
+	minimum = config[:min_q].to_i
+	metric_calculation = weekend_usage.to_f * (config[:peak_metric].to_i/config[:metric].to_i)
+	metric_calculation < minimum ? weekend_instances = minimum : weekend_instances = metric_calculation
+	total_weekend_hours = weekend_days	* 24 * weekend_instances
+	puts "Normal Hours: #{days_of_week} #{daily_instance_hours * days_of_week} Weekend Hours: #{weekend_days} #{total_weekend_hours}"
+	total_instance_hours_per_week = total_weekend_hours + daily_instance_hours * days_of_week
+	puts "total hours: #{total_instance_hours_per_week}"
+	puts "weekend instances per hour: #{weekend_instances}"
+	return total_instance_hours_per_week
+end
+end
